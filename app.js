a35003ec21556204eac54630b713b141735fa2ef
Jesse Li <a933476@gmail.com>
2020-06-06 19:43:37 +0800

    BIS home page set

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
+const bisRouter = require('./routes/bis');
 
 var app = express();
 
@@ -30,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/catalog', catalogRouter);
-
+app.use('/bis', bisRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
